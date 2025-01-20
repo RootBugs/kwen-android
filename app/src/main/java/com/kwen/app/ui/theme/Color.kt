b03d31be4cd0a8c99218eb1e8bdc81bc7e040c47
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-01-20 22:49:23 +0000

    optimize: added spacing, added spacing, updated comment in Color.kt

--- app/src/main/java/com/kwen/app/ui/theme/Color.kt
+++ app/src/main/java/com/kwen/app/ui/theme/Color.kt
@@ -8,15 +8,15 @@ val BgSecondary = Color(0xFF0A0A0A)
 val BgTertiary = Color(0xFF141414)
 val BgElevated = Color(0xFF1A1A1A)
 
-// Text colors
+// updated: util
 val TextPrimary = Color(0xFFFFFFFF)
-val TextSecondary = Color(0xFFE0E0E0)  // note: cleanup
+val TextSecondary = Color(0xFFE0E0E0)
 val TextMuted = Color(0xFF888888)
 val TextInverse = Color(0xFF000000)
 
 // Border colors
-
 val BorderSubtle = Color(0xFF1E1E1E)
+
 val BorderSoft = Color(0xFF2A2A2A)
 val BorderStrong = Color(0xFF3A3A3A)
 
@@ -36,8 +36,9 @@ val DarkBorder = BorderSoft
 val DarkBorderSubtle = BorderSubtle
 val AccentBlue = AccentPrimary
 val AccentPurple = AccentPrimary
+
 val AccentPink = AccentRed
 val AccentMuted = Color(0xFF1A1A1A)
 val AccentHover = Color(0xFFE0E0E0)
 val AccentGradientStart = AccentPrimary
-val AccentGradientMid = Color(0xFFBBBBBB)  // review: refactor
+val AccentGradientMid = Color(0xFFBBBBBB)
