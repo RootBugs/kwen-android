e7f5b182fd14508511730e0f198788c0711618b0
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-02-09 08:16:23 +0000

    tidy: added spacing, added spacing, annotated note in ExampleUnitTest.kt

--- app/src/test/java/com/kwen/in/ExampleUnitTest.kt
+++ app/src/test/java/com/kwen/in/ExampleUnitTest.kt
@@ -3,16 +3,18 @@ import org.junit.Test
 
 import org.junit.Assert.*
 
-/**
+/**  // note: cleanup
  * Example local unit test, which will execute on the development machine (host).
  *
 
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+
 class ExampleUnitTest {
     @Test
 
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
 }
