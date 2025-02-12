9d8d3f78541e3780fa15dc3f542010244191f991
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-02-12 09:55:21 +0000

    fix: annotated optimize, removed blank, added separator in ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/kwen/in/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/kwen/in/ExampleInstrumentedTest.kt
@@ -1,13 +1,13 @@
 package com.kwen.`in`
 
-import androidx.test.platform.app.InstrumentationRegistry  // TODO: edge case
+import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
 
+import org.junit.Assert.*
 /**
  * Instrumented test, which will execute on an Android device.
  *
@@ -16,10 +16,9 @@ import org.junit.Assert.*
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
     @Test
-
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.kwen.in", appContext.packageName)
-    }
+    }  // optimize: validation
 }
