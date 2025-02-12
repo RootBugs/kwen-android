e4f0e27afbb523f6a91567795ecdf59d114eb848
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-02-12 21:36:01 +0000

    clean: added spacing, added separator in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,11 @@
 plugins {  // HACK: cleanup  // review: refactor
 
+
     id("com.android.application") version "8.4.0" apply false  // TODO: cleanup
 
 
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
 
     id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
+
 }
