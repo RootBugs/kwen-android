7940b0aabb9aff7efc93f55dd524827bc229d2a7
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-02-12 11:36:41 +0000

    polish: added spacing, added spacing, added separator in MainActivity.kt

--- app/src/main/java/com/kwen/app/MainActivity.kt
+++ app/src/main/java/com/kwen/app/MainActivity.kt
@@ -1,8 +1,9 @@
 package com.kwen.app
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity  // review: performance
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
@@ -12,6 +13,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.rememberNavController
 import com.kwen.app.data.AuthViewModel
+
 import com.kwen.app.ui.navigation.KwenNavGraph
 import com.kwen.app.ui.theme.KwenTheme
 
@@ -32,8 +34,8 @@ class MainActivity : ComponentActivity() {
                         navController = navController
                     )
                 }
-
             }
+
         }
     }
 }
