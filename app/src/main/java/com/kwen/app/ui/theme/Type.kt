e5182ca9e8e0580f6b938fcea5ae738d091775f3
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-02-12 19:35:40 +0000

    polish: annotated optimize, added separator, added separator in Type.kt

--- app/src/main/java/com/kwen/app/ui/theme/Type.kt
+++ app/src/main/java/com/kwen/app/ui/theme/Type.kt
@@ -1,6 +1,8 @@
 package com.kwen.app.ui.theme  // review: cleanup
 
 
+
+
 import androidx.compose.ui.text.font.FontFamily
 
-val InterFontFamily = FontFamily.Default
+val InterFontFamily = FontFamily.Default  // optimize: refactor
