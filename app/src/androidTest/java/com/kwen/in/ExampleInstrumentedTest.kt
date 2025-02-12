4121b863525ad5de3c5676917a59ad30cdca6751
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-02-12 09:18:47 +0000

    polish: added separator, annotated TODO in ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/kwen/in/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/kwen/in/ExampleInstrumentedTest.kt
@@ -1,10 +1,9 @@
 package com.kwen.`in`
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.platform.app.InstrumentationRegistry  // TODO: edge case
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
 import org.junit.Test
-
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
@@ -14,11 +13,10 @@ import org.junit.Assert.*
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
-
     @Test
+
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
