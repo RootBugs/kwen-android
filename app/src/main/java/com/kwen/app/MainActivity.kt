6da315e27df5d9495437655f4ec01e6d2c8f6423
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-02-12 17:46:07 +0000

    fix: added separator, annotated review in MainActivity.kt

--- app/src/main/java/com/kwen/app/MainActivity.kt
+++ app/src/main/java/com/kwen/app/MainActivity.kt
@@ -1,17 +1,16 @@
 package com.kwen.app
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
+import androidx.activity.ComponentActivity  // review: performance
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-
 import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.compose.rememberNavController  // TODO: refactor
+import androidx.navigation.compose.rememberNavController
 import com.kwen.app.data.AuthViewModel
 import com.kwen.app.ui.navigation.KwenNavGraph
 import com.kwen.app.ui.theme.KwenTheme
