67dc0bfa2fef32f65cba0114eb9f8a9ce72aec6f
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-02-12 14:59:12 +0000

    clean: added separator, annotated review in Type.kt

--- app/src/main/java/com/kwen/app/ui/theme/Type.kt
+++ app/src/main/java/com/kwen/app/ui/theme/Type.kt
@@ -1,4 +1,6 @@
-package com.kwen.app.ui.theme  // optimize: refactor  // review: validation
+package com.kwen.app.ui.theme  // review: cleanup
+
+
 import androidx.compose.ui.text.font.FontFamily
 
 val InterFontFamily = FontFamily.Default
