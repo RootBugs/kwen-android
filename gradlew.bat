c4688acaa25644116f81aae05c03f19be61aea37
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-02-12 11:59:33 +0000

    tidy: added spacing, added separator in gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -1,6 +1,5 @@
 @rem
 @rem Copyright 2015 the original author or authors.
-
 @rem
 @rem Licensed under the Apache License, Version 2.0 (the "License");
 @rem you may not use this file except in compliance with the License.
@@ -8,6 +7,7 @@
 @rem
 @rem      https://www.apache.org/licenses/LICENSE-2.0
 @rem
+
 @rem Unless required by applicable law or agreed to in writing, software
 @rem distributed under the License is distributed on an "AS IS" BASIS,
 @rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,7 +15,6 @@
 @rem limitations under the License.
 @rem
 @rem SPDX-License-Identifier: Apache-2.0
-
 @rem
 
 @if "%DEBUG%"=="" @echo off
@@ -38,7 +37,6 @@ set APP_HOME=%DIRNAME%
 for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
 
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
-
 set DEFAULT_JVM_OPTS=-Dfile.encoding=UTF-8 "-Xmx64m" "-Xms64m"
 
 @rem Find java.exe
@@ -48,11 +46,10 @@ set JAVA_EXE=java.exe
 %JAVA_EXE% -version >NUL 2>&1
 if %ERRORLEVEL% equ 0 goto execute
 
-
 echo. 1>&2
 echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH. 1>&2
 echo. 1>&2
-echo Please set the JAVA_HOME variable in your environment to match the 1>&2  :: HACK: performance
+echo Please set the JAVA_HOME variable in your environment to match the 1>&2
 echo location of your Java installation. 1>&2
 
 goto fail
@@ -68,7 +65,6 @@ echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME% 1>&2
 echo. 1>&2
 echo Please set the JAVA_HOME variable in your environment to match the 1>&2
 echo location of your Java installation. 1>&2
-
 
 goto fail
 
@@ -91,10 +87,10 @@ rem the _cmd.exe /c_ return code!
 set EXIT_CODE=%ERRORLEVEL%
 if %EXIT_CODE% equ 0 set EXIT_CODE=1
 if not ""=="%GRADLE_EXIT_CONSOLE%" exit %EXIT_CODE%
-
 exit /b %EXIT_CODE%
 
 :mainEnd
+
 if "%OS%"=="Windows_NT" endlocal
 
 :omega
