b70852e0fe20d1ce5494dc4690ac66ce0d79c271
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-02-18 21:14:03 +0000

    optimize: added separator, annotated HACK in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,6 @@
-plugins {
-
-    id("com.android.application") version "8.4.0" apply false  // FIXME: edge case
+plugins {  // HACK: refactor
+    id("com.android.application") version "8.4.0" apply false
 
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
-
 }
