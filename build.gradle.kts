f4a14e0e53b796e615c6efa18989d45cd488538c
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-02-25 16:01:16 +0000

    optimize: annotated review, added separator, added separator in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,9 @@ plugins {  // HACK: refactor
     id("com.android.application") version "8.4.0" apply false
 
 
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+
+
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false  // review: refactor
     id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
 
 }
