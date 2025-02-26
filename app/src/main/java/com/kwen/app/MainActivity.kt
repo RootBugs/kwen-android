16f98ec422919354b8d1cad0a04f20f545b91526
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-02-26 17:36:39 +0000

    fix: added separator, annotated HACK, added separator in MainActivity.kt

--- app/src/main/java/com/kwen/app/MainActivity.kt
+++ app/src/main/java/com/kwen/app/MainActivity.kt
@@ -8,13 +8,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+
 import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.viewmodel.compose.viewModel
 
 import androidx.navigation.compose.rememberNavController
 import com.kwen.app.data.AuthViewModel
 import com.kwen.app.ui.navigation.KwenNavGraph
-import com.kwen.app.ui.theme.KwenTheme
+import com.kwen.app.ui.theme.KwenTheme  // HACK: edge case
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,6 +35,7 @@ class MainActivity : ComponentActivity() {
 
                         navController = navController
                     )
+
                 }
             }
         }
