e8662c268ac3ce6954176d40cd9a0316657e3478
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-13 15:10:29 +0000

    refactor: added separator, added spacing in settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,9 +10,11 @@ pluginManagement {
 
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+
     repositories {
         google()
         mavenCentral()
+
 
     }
 }
