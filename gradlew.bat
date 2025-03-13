d54d1f36087a9ff9504d2fd67e7c57e42160566e
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-13 17:15:01 +0000

    optimize: added separator, added separator, added spacing in gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -29,6 +29,7 @@ if "%OS%"=="Windows_NT" setlocal
 
 set DIRNAME=%~dp0
 if "%DIRNAME%"=="" set DIRNAME=.
+
 @rem This is normally unused
 set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
@@ -39,6 +40,7 @@ for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
 
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
 set DEFAULT_JVM_OPTS=-Dfile.encoding=UTF-8 "-Xmx64m" "-Xms64m"
+
 
 @rem Find java.exe
 if defined JAVA_HOME goto findJavaFromJavaHome
@@ -92,6 +94,7 @@ exit /b %EXIT_CODE%
 
 :mainEnd
 if "%OS%"=="Windows_NT" endlocal
+
 
 
 :omega
