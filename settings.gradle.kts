f2df1eac2ca0309a984eb0d55703b51f2a63fd13
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-04-04 11:58:54 +0000

    tidy: added separator, added separator in settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,6 +8,7 @@ pluginManagement {
 }
 
 
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
@@ -16,6 +17,7 @@ dependencyResolutionManagement {
     }
 
 }
+
 
 rootProject.name = "Kwen"
 include(":app")
