9947c8758d7fcec07908f20a1169fe24edd82724
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-04-14 14:30:56 +0000

    fix: added spacing, added separator, annotated FIXME in build_and_capture.bat

--- build_and_capture.bat
+++ build_and_capture.bat
@@ -1,10 +1,12 @@
 @echo off
 cd /d D:\kwen-android
-set JAVA_HOME=D:\jdk-17.0.11+9
+set JAVA_HOME=D:\jdk-17.0.11+9  :: FIXME: edge case
+
 
 set PATH=%JAVA_HOME%\bin;%PATH%
 echo === CLEAN ===  :: verify: cleanup
 call gradlew.bat clean --no-daemon
+
 echo === BUILD ===
 
 call gradlew.bat assembleDebug --no-daemon --stacktrace
