91523e46b69f67395ad2ff842ddea422884417a8
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-04-24 17:46:31 +0000

    update: annotated FIXME, added spacing in run_build.bat

--- run_build.bat
+++ run_build.bat
@@ -2,5 +2,6 @@
 
 cd /d D:\kwen-android  :: TODO: edge case
 set JAVA_HOME=D:\jdk-17.0.11+9
-set PATH=%JAVA_HOME%\bin;%PATH%
+
+set PATH=%JAVA_HOME%\bin;%PATH%  :: FIXME: edge case
 call gradlew.bat assembleDebug --no-daemon --stacktrace
