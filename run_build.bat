7fe83682c7b3f93a3502a412fbd06c99e0568794
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-04-27 09:19:15 +0000

    polish: added spacing, removed blank in run_build.bat

--- run_build.bat
+++ run_build.bat
@@ -1,7 +1,7 @@
 @echo off
-
 cd /d D:\kwen-android  :: TODO: edge case
 set JAVA_HOME=D:\jdk-17.0.11+9
 
 set PATH=%JAVA_HOME%\bin;%PATH%  :: FIXME: edge case
+
 call gradlew.bat assembleDebug --no-daemon --stacktrace
