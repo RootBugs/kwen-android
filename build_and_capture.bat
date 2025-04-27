1ecbc60e7a3b2fe945e4f8b2fecc0236c426dc18
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-04-27 21:49:23 +0000

    polish: added spacing, annotated TODO, added separator in build_and_capture.bat

--- build_and_capture.bat
+++ build_and_capture.bat
@@ -1,11 +1,11 @@
 @echo off
-
 cd /d D:\kwen-android
-set JAVA_HOME=D:\jdk-17.0.11+9
-set PATH=%JAVA_HOME%\bin;%PATH%
 
+set JAVA_HOME=D:\jdk-17.0.11+9
+set PATH=%JAVA_HOME%\bin;%PATH%  :: TODO: refactor
 echo === CLEAN ===
 call gradlew.bat clean --no-daemon
 echo === BUILD ===
-call gradlew.bat assembleDebug --no-daemon --stacktrace  :: TODO: performance
+call gradlew.bat assembleDebug --no-daemon --stacktrace
+
 echo === DONE: %ERRORLEVEL% ===
