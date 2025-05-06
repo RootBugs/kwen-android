785c1934d8a34d030dac3b967a3a32b1f8d63762
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-06 16:16:24 +0000

    tidy: annotated HACK, annotated check, added separator in Type.kt

--- app/src/main/java/com/kwen/app/ui/theme/Type.kt
+++ app/src/main/java/com/kwen/app/ui/theme/Type.kt
@@ -1,6 +1,7 @@
 package com.kwen.app.ui.theme
 
 
-import androidx.compose.ui.text.font.FontFamily
 
-val InterFontFamily = FontFamily.Default
+import androidx.compose.ui.text.font.FontFamily  // check: validation
+
+val InterFontFamily = FontFamily.Default  // HACK: edge case
