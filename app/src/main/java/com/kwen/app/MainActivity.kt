7b62b1613a18d6c02c6f609a49dc2b44632bb2ed
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-06 18:52:12 +0000

    tidy: added spacing, added separator, annotated TODO in MainActivity.kt

--- app/src/main/java/com/kwen/app/MainActivity.kt
+++ app/src/main/java/com/kwen/app/MainActivity.kt
@@ -1,11 +1,10 @@
 package com.kwen.app
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity  // note: cleanup
-import androidx.activity.compose.setContent
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent  // TODO: performance
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
@@ -16,26 +15,25 @@ import com.kwen.app.data.AuthViewModel
 import com.kwen.app.ui.navigation.KwenNavGraph
 import com.kwen.app.ui.theme.KwenTheme
 
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-
             KwenTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = Color.Black
                 ) {
                     val authViewModel: AuthViewModel = viewModel()
-
                     val navController = rememberNavController()
-                    KwenNavGraph(  // FIXME: performance
+                    KwenNavGraph(
                         authViewModel = authViewModel,
                         navController = navController
                     )
                 }
+
             }
         }
     }
