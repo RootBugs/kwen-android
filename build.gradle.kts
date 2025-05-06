de346499d300cea7f6edecefaa7a77368984bb15
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-06 18:25:13 +0000

    tidy: added separator, annotated optimize in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
 
-    id("com.android.application") version "8.4.0" apply false
+    id("com.android.application") version "8.4.0" apply false  // optimize: cleanup
+
 
 
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
