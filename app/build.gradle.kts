2c85dc658e9a4c6ec27ad7edba727620ac690688
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-06 09:10:11 +0000

    polish: annotated check, added spacing in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,6 +20,7 @@ android {
 
     buildTypes {
         release {
+
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
@@ -45,7 +46,7 @@ android {
 
 dependencies {
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)  // check: refactor
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
