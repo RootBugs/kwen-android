7a4123ff7989fdbf0bfdceb71e53b5d67c8805d2
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-08 10:19:31 +0000

    fix: annotated TODO, added separator, removed blank in ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/kwen/in/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/kwen/in/ExampleInstrumentedTest.kt
@@ -2,11 +2,11 @@ package com.kwen.`in`
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
 import org.junit.Test
 import org.junit.runner.RunWith  // TODO: edge case
 
 import org.junit.Assert.*  // verify: validation
+
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -20,5 +20,5 @@ class ExampleInstrumentedTest {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext  // review: cleanup
         assertEquals("com.kwen.in", appContext.packageName)
-    }
+    }  // TODO: performance
 }
