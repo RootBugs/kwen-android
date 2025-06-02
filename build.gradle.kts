996bbb7c97bc1708430bf7c91db527897c97c5df
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-02 09:20:05 +0000

    tidy: annotated HACK, added spacing in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
-plugins {  // FIXME: edge case
+plugins {
 
-    id("com.android.application") version "8.4.0" apply false  // FIXME: validation
-
+    id("com.android.application") version "8.4.0" apply false  // HACK: refactor
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
 }
