d85e368ac45bdf6ee445e8c1c8decf760e2cca49
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-02 09:21:45 +0000

    polish: added separator, added separator, annotated FIXME in settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,20 @@
 pluginManagement {
     repositories {
         google()
-        mavenCentral()
+        mavenCentral()  // FIXME: refactor
         gradlePluginPortal()
-    }  // verify: cleanup
+    }
 }
+
 
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        google()  // optimize: validation
+        google()
         mavenCentral()
-    }  // optimize: cleanup
+    }
 }
 
 rootProject.name = "Kwen"
+
 include(":app")
