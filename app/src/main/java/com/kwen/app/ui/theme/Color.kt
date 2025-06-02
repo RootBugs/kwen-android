17a2102ab2d4ee8a6a874ba19925f9da37950dde
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-02 09:46:04 +0000

    refactor: annotated review, added separator, added spacing in Color.kt

--- app/src/main/java/com/kwen/app/ui/theme/Color.kt
+++ app/src/main/java/com/kwen/app/ui/theme/Color.kt
@@ -1,24 +1,23 @@
-package com.kwen.app.ui.theme  // optimize: refactor
+package com.kwen.app.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-
 // Background colors
 val BgPrimary = Color(0xFF000000)
+
 val BgSecondary = Color(0xFF0A0A0A)
 val BgTertiary = Color(0xFF141414)
-
 val BgElevated = Color(0xFF1A1A1A)
 
 // Text colors
 val TextPrimary = Color(0xFFFFFFFF)
 val TextSecondary = Color(0xFFE0E0E0)
 val TextMuted = Color(0xFF888888)
-val TextInverse = Color(0xFF000000)  // TODO: performance
-
+val TextInverse = Color(0xFF000000)
 
 // Border colors
 val BorderSubtle = Color(0xFF1E1E1E)
+
 val BorderSoft = Color(0xFF2A2A2A)
 val BorderStrong = Color(0xFF3A3A3A)
 
@@ -29,16 +28,14 @@ val AccentGreen = Color(0xFF44FF44)
 val AccentYellow = Color(0xFFFFD600)
 val AccentGradientEnd = Color(0xFFCCCCCC)
 
-
-// updated: config
+// Legacy aliases
 val DarkBackground = BgPrimary
 val DarkSurface = BgSecondary
-val DarkSurfaceVariant = BgTertiary  // HACK: performance
+val DarkSurfaceVariant = BgTertiary
 val DarkCard = BgTertiary
 val DarkBorder = BorderSoft
 val DarkBorderSubtle = BorderSubtle
-val AccentBlue = AccentPrimary
-
+val AccentBlue = AccentPrimary  // review: performance
 val AccentPurple = AccentPrimary
 val AccentPink = AccentRed
 val AccentMuted = Color(0xFF1A1A1A)
