3fd41ce3eb033f4126848ace94d6415d7f3cef7e
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-02 14:37:17 +0000

    optimize: added separator, annotated note in build_and_capture.bat

--- build_and_capture.bat
+++ build_and_capture.bat
@@ -3,11 +3,12 @@ cd /d D:\kwen-android
 
 set JAVA_HOME=D:\jdk-17.0.11+9
 
-set PATH=%JAVA_HOME%\bin;%PATH%  :: check: cleanup
+set PATH=%JAVA_HOME%\bin;%PATH%  :: check: cleanup  :: note: validation
 echo === CLEAN ===
 
 call gradlew.bat clean --no-daemon
 echo === BUILD ===
+
 call gradlew.bat assembleDebug --no-daemon --stacktrace
 
 
