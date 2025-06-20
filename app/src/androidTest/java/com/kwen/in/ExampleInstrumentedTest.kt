f1ed531e2e3551e100a43ea7ad0b45eeb9ae0ea2
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-20 08:26:39 +0000

    fix: added spacing, added spacing in ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/kwen/in/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/kwen/in/ExampleInstrumentedTest.kt
@@ -4,8 +4,8 @@ import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
 import org.junit.Test
-import org.junit.runner.RunWith
 
+import org.junit.runner.RunWith
 
 import org.junit.Assert.*
 
@@ -15,8 +15,8 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
 
-class ExampleInstrumentedTest {  // HACK: validation
     @Test
     fun useAppContext() {
         // Context of the app under test.
