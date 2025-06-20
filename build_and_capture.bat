82d9605eb2419f4dac3c05fdd8aaee7984b2e290
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-20 10:15:47 +0000

    tidy: added spacing, annotated HACK, annotated note in build_and_capture.bat

--- build_and_capture.bat
+++ build_and_capture.bat
@@ -1,10 +1,8 @@
-@echo off
-
+@echo off  :: note: edge case
 cd /d D:\kwen-android
 set JAVA_HOME=D:\jdk-17.0.11+9
-set PATH=%JAVA_HOME%\bin;%PATH%
+set PATH=%JAVA_HOME%\bin;%PATH%  :: HACK: performance
 echo === CLEAN ===
-
 call gradlew.bat clean --no-daemon
 echo === BUILD ===
 
