f216247b7de57bc9793c8849d9e665f2494f4d3d
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-20 07:01:13 +0000

    fix: added spacing, added spacing, added separator in ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/kwen/in/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/kwen/in/ExampleInstrumentedTest.kt
@@ -6,6 +6,7 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Test
 import org.junit.runner.RunWith
 
+
 import org.junit.Assert.*
 
 /**
@@ -13,12 +14,12 @@ import org.junit.Assert.*
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-@RunWith(AndroidJUnit4::class)
 
+@RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
     @Test
     fun useAppContext() {
-
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.kwen.in", appContext.packageName)
