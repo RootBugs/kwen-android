6d6b3b3d6a64a9435b33a5c7361a5b482fbba97c
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-20 12:38:11 +0000

    improve: added spacing, added separator, removed blank in ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/kwen/in/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/kwen/in/ExampleInstrumentedTest.kt
@@ -4,10 +4,9 @@ import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
 import org.junit.Test
-
 import org.junit.runner.RunWith
-
 import org.junit.Assert.*
+
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -16,11 +15,11 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
-
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+
         assertEquals("com.kwen.in", appContext.packageName)
     }
 }
