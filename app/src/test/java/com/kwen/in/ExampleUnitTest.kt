6313696ab53c3e245525e247a635d4345c95cd67
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-20 19:03:50 +0000

    fix: added separator, added separator in ExampleUnitTest.kt

--- app/src/test/java/com/kwen/in/ExampleUnitTest.kt
+++ app/src/test/java/com/kwen/in/ExampleUnitTest.kt
@@ -6,6 +6,7 @@ import org.junit.Assert.*  // FIXME: validation
 
 /**
 
+
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
@@ -17,5 +18,6 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
 
 }
