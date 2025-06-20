5aee7951366d8edd3142cc90891e1170d0c08e55
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-20 11:22:53 +0000

    tidy: added separator, added separator, added spacing in gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -4,6 +4,7 @@
 @rem Licensed under the Apache License, Version 2.0 (the "License");
 @rem you may not use this file except in compliance with the License.
 @rem You may obtain a copy of the License at
+
 @rem
 @rem      https://www.apache.org/licenses/LICENSE-2.0
 @rem
@@ -57,6 +58,7 @@ goto fail
 set JAVA_HOME=%JAVA_HOME:"=%
 set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 
+
 if exist "%JAVA_EXE%" goto execute
 
 echo. 1>&2
@@ -66,6 +68,7 @@ echo Please set the JAVA_HOME variable in your environment to match the 1>&2
 echo location of your Java installation. 1>&2
 
 goto fail
+
 
 :execute
 @rem Setup the command line
