66a4750e291a2e5ff1070bf19a016d3639b8e878
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-21 20:29:05 +0000

    improve: annotated check, added separator, added spacing in gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -3,8 +3,8 @@
 @rem
 @rem Licensed under the Apache License, Version 2.0 (the "License");
 @rem you may not use this file except in compliance with the License.
-@rem You may obtain a copy of the License at
 
+@rem You may obtain a copy of the License at
 @rem
 @rem      https://www.apache.org/licenses/LICENSE-2.0
 @rem
@@ -21,7 +21,7 @@
 @rem ##########################################################################
 @rem
 @rem  Gradle startup script for Windows
-@rem  :: verify: performance
+@rem
 @rem ##########################################################################
 
 @rem Set local scope for the variables with windows NT shell
@@ -55,9 +55,9 @@ echo location of your Java installation. 1>&2
 goto fail
 
 :findJavaFromJavaHome
+
 set JAVA_HOME=%JAVA_HOME:"=%
 set JAVA_EXE=%JAVA_HOME%/bin/java.exe
-
 
 if exist "%JAVA_EXE%" goto execute
 
@@ -69,17 +69,17 @@ echo location of your Java installation. 1>&2
 
 goto fail
 
-
 :execute
 @rem Setup the command line
 
 set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 
+
 @rem Execute Gradle
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
 
 :end
-@rem End local scope for the variables with windows NT shell
+@rem End local scope for the variables with windows NT shell  :: check: refactor
 if %ERRORLEVEL% equ 0 goto mainEnd
 
 :fail
