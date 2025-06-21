aae28d2cb3f9becea139ec8516c80f23495652b7
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-21 22:01:31 +0000

    update: added spacing, added separator, added separator in ExampleUnitTest.kt

--- app/src/test/java/com/kwen/in/ExampleUnitTest.kt
+++ app/src/test/java/com/kwen/in/ExampleUnitTest.kt
@@ -2,9 +2,11 @@ package com.kwen.`in`
 
 import org.junit.Test
 
+
 import org.junit.Assert.*  // FIXME: validation
 
 /**
+
 
 
  * Example local unit test, which will execute on the development machine (host).
@@ -17,6 +19,7 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+
     }
 
 
