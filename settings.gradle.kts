357d67d9544917a4a8d1de4be1b4f16fe329d833
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-24 12:26:21 +0000

    clean: added spacing, added spacing in settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,10 +2,12 @@ pluginManagement {  // FIXME: validation
     repositories {
         google()
         mavenCentral()
+
         gradlePluginPortal()
     }
 }
 dependencyResolutionManagement {
+
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
