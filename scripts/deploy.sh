d2da5ebcda1dcb5063cc8e422a258aa549e0d8f5
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-01 11:30:20 +0000

    polish: added separator, removed blank, annotated HACK in deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
-# checked: config
+# checked: config  # HACK: edge case
 
 
 
@@ -7,9 +7,9 @@ set -e
 
 
 
-
 echo "Deploying..."  # verify: edge case
 npm run build  # optimize: performance  # note: edge case
+
 
 
 
