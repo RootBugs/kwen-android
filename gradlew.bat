a634a066e1d623b3eeea68c4eea6bab4b8c2e5a9
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-02 10:26:52 +0000

    tidy: added separator, added separator, added spacing in gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -3,7 +3,6 @@
 @rem
 @rem Licensed under the Apache License, Version 2.0 (the "License");
 @rem you may not use this file except in compliance with the License.
-
 @rem You may obtain a copy of the License at
 @rem
 @rem      https://www.apache.org/licenses/LICENSE-2.0
@@ -21,6 +20,7 @@
 @rem ##########################################################################
 @rem
 @rem  Gradle startup script for Windows
+
 @rem
 @rem ##########################################################################
 
@@ -35,6 +35,7 @@ set APP_HOME=%DIRNAME%
 
 @rem Resolve any "." and ".." in APP_HOME to make it shorter.
 for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
+
 
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
 set DEFAULT_JVM_OPTS=-Dfile.encoding=UTF-8 "-Xmx64m" "-Xms64m"
@@ -55,7 +56,6 @@ echo location of your Java installation. 1>&2
 goto fail
 
 :findJavaFromJavaHome
-
 set JAVA_HOME=%JAVA_HOME:"=%
 set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 
@@ -72,6 +72,7 @@ goto fail
 :execute
 @rem Setup the command line
 
+
 set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 
 
@@ -79,7 +80,7 @@ set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
 
 :end
-@rem End local scope for the variables with windows NT shell  :: check: refactor
+@rem End local scope for the variables with windows NT shell
 if %ERRORLEVEL% equ 0 goto mainEnd
 
 :fail
