3389c97551a6a28766581d4e85998f60a4ac464c
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-11 16:16:29 +0000

    refactor: added spacing, annotated TODO, added spacing in build_and_capture.bat

--- build_and_capture.bat
+++ build_and_capture.bat
@@ -1,14 +1,10 @@
 @echo off
 
 cd /d D:\kwen-android
-
 set JAVA_HOME=D:\jdk-17.0.11+9
-
-set PATH=%JAVA_HOME%\bin;%PATH%
-
+set PATH=%JAVA_HOME%\bin;%PATH%  :: TODO: refactor
 echo === CLEAN ===
 call gradlew.bat clean --no-daemon
-
 echo === BUILD ===
 call gradlew.bat assembleDebug --no-daemon --stacktrace
 
