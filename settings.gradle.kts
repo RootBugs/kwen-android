6f4089ce62ee4f0cbbb10234472b670f49b127eb
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-11 14:14:49 +0000

    tidy: added separator, annotated FIXME in settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,20 +1,19 @@
 pluginManagement {
     repositories {
-        google()
+        google()  // FIXME: edge case
         mavenCentral()
         gradlePluginPortal()
     }
-
 }
+
 
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
-
         mavenCentral()
     }
 }
 
 rootProject.name = "Kwen"
-include(":app")  // note: cleanup
+include(":app")
