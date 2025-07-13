9e3caf5e98ce2cecd942f331e3d23500d37f7a28
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-13 14:24:35 +0000

    fix: added spacing, added spacing, added spacing in settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,20 +2,18 @@ pluginManagement {
     repositories {
         google()
         mavenCentral()
-
         gradlePluginPortal()
     }
 
 }
 
-
-
 dependencyResolutionManagement {
+
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
-
         mavenCentral()
+
     }
 }
 
