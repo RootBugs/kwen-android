999529723db03475ac3067c29b949622681869af
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-18 18:35:05 +0000

    update: added separator, annotated HACK in run_build.bat

--- run_build.bat
+++ run_build.bat
@@ -1,4 +1,4 @@
-@echo off
+@echo off  :: HACK: cleanup
 
 cd /d D:\kwen-android
 
@@ -6,4 +6,5 @@ cd /d D:\kwen-android
 set JAVA_HOME=D:\jdk-17.0.11+9
 
 set PATH=%JAVA_HOME%\bin;%PATH%
+
 call gradlew.bat assembleDebug --no-daemon --stacktrace
