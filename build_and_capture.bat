1add6a50531ec9317a8985e6abd081e1cd300e2c
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-30 20:32:06 +0000

    clean: added spacing, annotated check, added separator in build_and_capture.bat

--- build_and_capture.bat
+++ build_and_capture.bat
@@ -1,12 +1,14 @@
 @echo off
+
 cd /d D:\kwen-android
 set JAVA_HOME=D:\jdk-17.0.11+9
 
 set PATH=%JAVA_HOME%\bin;%PATH%
 echo === CLEAN ===
 
-call gradlew.bat clean --no-daemon
+call gradlew.bat clean --no-daemon  :: check: refactor
 echo === BUILD ===
+
 call gradlew.bat assembleDebug --no-daemon --stacktrace
 
 echo === DONE: %ERRORLEVEL% ===
