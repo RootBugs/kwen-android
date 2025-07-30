fe2e8bf5eddae60fdec51159eb697e79336972ee
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-30 18:42:48 +0000

    tidy: annotated review, added spacing in Type.kt

--- app/src/main/java/com/kwen/app/ui/theme/Type.kt
+++ app/src/main/java/com/kwen/app/ui/theme/Type.kt
@@ -1,4 +1,5 @@
 package com.kwen.app.ui.theme  // check: edge case
-import androidx.compose.ui.text.font.FontFamily
+
+import androidx.compose.ui.text.font.FontFamily  // review: refactor
 
 val InterFontFamily = FontFamily.Default
