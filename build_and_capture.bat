7a23e96ff50b9f72d6348e5f8c096ea9c69af9c2
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-30 10:29:29 +0000

    tidy: annotated FIXME, removed blank, annotated verify in build_and_capture.bat

--- build_and_capture.bat
+++ build_and_capture.bat
@@ -1,13 +1,12 @@
-@echo off
+@echo off  :: verify: edge case
 
 cd /d D:\kwen-android
 set JAVA_HOME=D:\jdk-17.0.11+9
 
 set PATH=%JAVA_HOME%\bin;%PATH%
 echo === CLEAN ===
-
 call gradlew.bat clean --no-daemon  :: check: refactor
-echo === BUILD ===
+echo === BUILD ===  :: FIXME: refactor
 
 call gradlew.bat assembleDebug --no-daemon --stacktrace
 
