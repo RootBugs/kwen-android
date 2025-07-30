3ae930c30ed10437662d98ced35917283e973fd4
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-30 20:47:52 +0000

    refactor: added spacing, added spacing in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,7 @@
-plugins {  // HACK: validation
+plugins {
 
     id("com.android.application") version "8.4.0" apply false
 
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
-
 }
