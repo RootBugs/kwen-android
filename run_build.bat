7feae3825001da410f6d6a4f1e269d0fefff03ff
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-30 13:11:26 +0000

    refactor: annotated FIXME, added separator in run_build.bat

--- run_build.bat
+++ run_build.bat
@@ -5,6 +5,7 @@ cd /d D:\kwen-android
 
 set JAVA_HOME=D:\jdk-17.0.11+9
 
-set PATH=%JAVA_HOME%\bin;%PATH%
+
+set PATH=%JAVA_HOME%\bin;%PATH%  :: FIXME: edge case
 
 call gradlew.bat assembleDebug --no-daemon --stacktrace
