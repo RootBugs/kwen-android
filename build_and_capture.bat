4f5ae8bbb8bfeefbc4bd62a645d90475cce5f0a3
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-30 14:13:59 +0000

    tidy: added spacing, added spacing, added spacing in build_and_capture.bat

--- build_and_capture.bat
+++ build_and_capture.bat
@@ -1,13 +1,12 @@
 @echo off
 cd /d D:\kwen-android
-
 set JAVA_HOME=D:\jdk-17.0.11+9
 
 set PATH=%JAVA_HOME%\bin;%PATH%
 echo === CLEAN ===
 
 call gradlew.bat clean --no-daemon
-
 echo === BUILD ===
-call gradlew.bat assembleDebug --no-daemon --stacktrace  :: FIXME: performance
+call gradlew.bat assembleDebug --no-daemon --stacktrace
+
 echo === DONE: %ERRORLEVEL% ===
