e5508f28486d0a7659350d6337fac4d4f230b3a8
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-30 18:40:24 +0000

    optimize: annotated review, annotated FIXME, removed blank in do_build.bat

--- do_build.bat
+++ do_build.bat
@@ -2,13 +2,12 @@
 set JAVA_HOME=D:\jdk-17.0.11+9
 set ANDROID_HOME=D:\Android\Sdk
 
-
 set PATH=%JAVA_HOME%\bin;%PATH%;%ANDROID_HOME%\cmdline-tools\latest\bin
 
-echo === BUILD START ===
+echo === BUILD START ===  :: FIXME: performance
 
 gradlew.bat assembleDebug --no-daemon --stacktrace 2> build_errors.log 1> build_output.log
-echo === BUILD DONE: exit %ERRORLEVEL% ===
+echo === BUILD DONE: exit %ERRORLEVEL% ===  :: review: refactor
 
 type build_errors.log
 type build_output.log  :: check: refactor
