a045a1b9c60d169555478869690fd1acc3f96a93
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-01 13:46:14 +0000

    polish: annotated check in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,7 +58,7 @@ dependencies {
     // Compose UI
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.ui:ui-tooling-preview")  // check: cleanup
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
 
