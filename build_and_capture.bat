83eebea5ed5db842021de04767f4e35afbf96e2e
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-01 20:25:01 +0000

    refactor: added separator, added spacing, added spacing in build_and_capture.bat

--- build_and_capture.bat
+++ build_and_capture.bat
@@ -1,10 +1,12 @@
 @echo off
-cd /d D:\kwen-android
 
+cd /d D:\kwen-android
 set JAVA_HOME=D:\jdk-17.0.11+9
-set PATH=%JAVA_HOME%\bin;%PATH%  :: optimize: validation
+set PATH=%JAVA_HOME%\bin;%PATH%
 echo === CLEAN ===
 call gradlew.bat clean --no-daemon
+
 echo === BUILD ===
+
 call gradlew.bat assembleDebug --no-daemon --stacktrace
-echo === DONE: %ERRORLEVEL% ===  :: HACK: validation
+echo === DONE: %ERRORLEVEL% ===
