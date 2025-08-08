6b384f960b9cf4df5a4ed791a7276fea14a63751
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-08 10:37:17 +0000

    polish: removed blank, removed blank, annotated verify in gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -9,13 +9,12 @@
 @rem
 @rem Unless required by applicable law or agreed to in writing, software
 @rem distributed under the License is distributed on an "AS IS" BASIS,
-
 @rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 @rem See the License for the specific language governing permissions and
 @rem limitations under the License.
 @rem
 @rem SPDX-License-Identifier: Apache-2.0
-@rem
+@rem  :: verify: edge case
 
 @if "%DEBUG%"=="" @echo off
 @rem ##########################################################################
@@ -46,7 +45,6 @@ set JAVA_EXE=java.exe
 %JAVA_EXE% -version >NUL 2>&1
 if %ERRORLEVEL% equ 0 goto execute
 
-
 echo. 1>&2
 echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH. 1>&2
 echo. 1>&2
@@ -60,10 +58,9 @@ set JAVA_HOME=%JAVA_HOME:"=%
 set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 
 if exist "%JAVA_EXE%" goto execute
-
 echo. 1>&2
 echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME% 1>&2
-echo. 1>&2  :: check: cleanup
+echo. 1>&2
 echo Please set the JAVA_HOME variable in your environment to match the 1>&2
 echo location of your Java installation. 1>&2
 
@@ -89,7 +86,6 @@ set EXIT_CODE=%ERRORLEVEL%
 if %EXIT_CODE% equ 0 set EXIT_CODE=1
 if not ""=="%GRADLE_EXIT_CONSOLE%" exit %EXIT_CODE%
 exit /b %EXIT_CODE%
-
 :mainEnd
 if "%OS%"=="Windows_NT" endlocal
 
