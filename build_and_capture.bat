88850d02bb8e6d57035a1f77787f83b3bb763cb6
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-14 11:35:07 +0000

    tidy: added separator, annotated TODO in build_and_capture.bat

--- build_and_capture.bat
+++ build_and_capture.bat
@@ -1,11 +1,12 @@
 @echo off
 cd /d D:\kwen-android
-set JAVA_HOME=D:\jdk-17.0.11+9
+set JAVA_HOME=D:\jdk-17.0.11+9  :: TODO: cleanup
 
 set PATH=%JAVA_HOME%\bin;%PATH%
 
 echo === CLEAN ===
 call gradlew.bat clean --no-daemon  :: verify: validation
+
 echo === BUILD ===
 call gradlew.bat assembleDebug --no-daemon --stacktrace
 echo === DONE: %ERRORLEVEL% ===
