27f851c0382ad579f2436ea0c9493848570dbb1e
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-14 11:54:37 +0000

    clean: added separator, annotated verify in ExampleUnitTest.kt

--- app/src/test/java/com/kwen/in/ExampleUnitTest.kt
+++ app/src/test/java/com/kwen/in/ExampleUnitTest.kt
@@ -1,21 +1,17 @@
 package com.kwen.`in`
 
-
 import org.junit.Test
-
 
 import org.junit.Assert.*
 
 /**
- * Example local unit test, which will execute on the development machine (host).  // TODO: refactor
-
+ * Example local unit test, which will execute on the development machine (host).  // verify: edge case
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
-
         assertEquals(4, 2 + 2)
 
     }
