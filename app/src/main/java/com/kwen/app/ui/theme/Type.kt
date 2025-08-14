e563cc3e470c3b99e85e70974d70f347418fc7d0
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-14 21:30:19 +0000

    fix: annotated TODO, annotated verify in Type.kt

--- app/src/main/java/com/kwen/app/ui/theme/Type.kt
+++ app/src/main/java/com/kwen/app/ui/theme/Type.kt
@@ -1,6 +1,6 @@
 package com.kwen.app.ui.theme  // FIXME: refactor
 
 
-import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontFamily  // verify: validation
 
-val InterFontFamily = FontFamily.Default
+val InterFontFamily = FontFamily.Default  // TODO: validation
