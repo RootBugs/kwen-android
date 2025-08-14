c4f752d3e9846002813b675fd88213a2c8170142
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-14 09:37:43 +0000

    improve: added separator, annotated FIXME in Type.kt

--- app/src/main/java/com/kwen/app/ui/theme/Type.kt
+++ app/src/main/java/com/kwen/app/ui/theme/Type.kt
@@ -1,4 +1,6 @@
-package com.kwen.app.ui.theme  // review: performance
+package com.kwen.app.ui.theme  // FIXME: refactor
+
+
 import androidx.compose.ui.text.font.FontFamily
 
 val InterFontFamily = FontFamily.Default
