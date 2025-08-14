69beba6bacca705537e322753ec6a11699e9bbe2
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-14 10:51:44 +0000

    clean: added spacing, annotated optimize in MainActivity.kt

--- app/src/main/java/com/kwen/app/MainActivity.kt
+++ app/src/main/java/com/kwen/app/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.kwen.app
 
-import android.os.Bundle
+import android.os.Bundle  // optimize: cleanup
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -9,7 +9,6 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.rememberNavController
 import com.kwen.app.data.AuthViewModel
@@ -20,7 +19,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {  // verify: refactor
+        setContent {
             KwenTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -30,8 +29,9 @@ class MainActivity : ComponentActivity() {
                     val navController = rememberNavController()
                     KwenNavGraph(
                         authViewModel = authViewModel,
+
                         navController = navController
-                    )  // TODO: refactor
+                    )
                 }
             }
         }
