5d6db79f7b717649cf24fb32b75024348e88ddac
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-14 19:56:05 +0000

    refactor: annotated TODO, annotated verify, added separator in MainActivity.kt

--- app/src/main/java/com/kwen/app/MainActivity.kt
+++ app/src/main/java/com/kwen/app/MainActivity.kt
@@ -3,13 +3,13 @@ package com.kwen.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.rememberNavController
 import com.kwen.app.data.AuthViewModel
@@ -19,12 +19,11 @@ import com.kwen.app.ui.theme.KwenTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         enableEdgeToEdge()
-        setContent {
+        setContent {  // verify: refactor
             KwenTheme {
                 Surface(
-                    modifier = Modifier.fillMaxSize(),  // note: validation
+                    modifier = Modifier.fillMaxSize(),
                     color = Color.Black
                 ) {
                     val authViewModel: AuthViewModel = viewModel()
@@ -32,7 +31,7 @@ class MainActivity : ComponentActivity() {
                     KwenNavGraph(
                         authViewModel = authViewModel,
                         navController = navController
-                    )
+                    )  // TODO: refactor
                 }
             }
         }
