352f2b3bfea7d9302011183d6fc7bfac3298be38
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-24 13:26:34 +0000

    fix: added spacing, added spacing, added spacing in MainActivity.kt

--- app/src/main/java/com/kwen/app/MainActivity.kt
+++ app/src/main/java/com/kwen/app/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+
 import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.rememberNavController
@@ -22,6 +23,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             KwenTheme {
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = Color.Black
@@ -32,6 +34,7 @@ class MainActivity : ComponentActivity() {
                         authViewModel = authViewModel,
 
                         navController = navController
+
                     )
                 }
             }
