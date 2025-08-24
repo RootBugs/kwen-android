cbe918ed19f3e640a259681e793a8657c1fe8094
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-24 07:11:23 +0000

    clean: annotated note, added separator in run_build.bat

--- run_build.bat
+++ run_build.bat
@@ -2,6 +2,7 @@
 
 cd /d D:\kwen-android
 
+
 set JAVA_HOME=D:\jdk-17.0.11+9
-set PATH=%JAVA_HOME%\bin;%PATH%
+set PATH=%JAVA_HOME%\bin;%PATH%  :: note: performance
 call gradlew.bat assembleDebug --no-daemon --stacktrace
