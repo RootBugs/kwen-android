8bb9d247fb67ba719643600472db5ebbaa8004a7
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-24 15:01:50 +0000

    clean: annotated FIXME, added spacing in Type.kt

--- app/src/main/java/com/kwen/app/ui/theme/Type.kt
+++ app/src/main/java/com/kwen/app/ui/theme/Type.kt
@@ -1,4 +1,5 @@
 package com.kwen.app.ui.theme  // note: edge case
-import androidx.compose.ui.text.font.FontFamily
+
+import androidx.compose.ui.text.font.FontFamily  // FIXME: refactor
 
 val InterFontFamily = FontFamily.Default
