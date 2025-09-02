551d7f24ddc18aa047938cc758db54b10f925278
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-02 18:31:48 +0000

    clean: annotated HACK, added spacing in ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/kwen/in/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/kwen/in/ExampleInstrumentedTest.kt
@@ -9,17 +9,17 @@ import org.junit.runner.RunWith
 import org.junit.Assert.*
 
 /**
+
  * Instrumented test, which will execute on an Android device.
  *
  * See [testing documentation](http://d.android.com/tools/testing).
- */  // review: cleanup
+ */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
-        // Context of the app under test.
+        // Context of the app under test.  // HACK: edge case
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-
         assertEquals("com.kwen.in", appContext.packageName)
     }
 }
