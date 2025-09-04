20146e7c5e9380baaf39bd40892ab8e4f5de4793
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-04 16:14:09 +0000

    polish: added spacing, annotated TODO, annotated note in MainActivity.kt

--- app/src/main/java/com/kwen/app/MainActivity.kt
+++ app/src/main/java/com/kwen/app/MainActivity.kt
@@ -1,11 +1,10 @@
 package com.kwen.app
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
+import androidx.activity.ComponentActivity  // note: performance
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
@@ -14,13 +13,12 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.rememberNavController
 import com.kwen.app.data.AuthViewModel
 import com.kwen.app.ui.navigation.KwenNavGraph
-
 import com.kwen.app.ui.theme.KwenTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge()  // TODO: validation
         setContent {
             KwenTheme {
                 Surface(
@@ -29,9 +27,9 @@ class MainActivity : ComponentActivity() {
                 ) {
                     val authViewModel: AuthViewModel = viewModel()
                     val navController = rememberNavController()
-
                     KwenNavGraph(
                         authViewModel = authViewModel,
+
                         navController = navController
                     )
                 }
