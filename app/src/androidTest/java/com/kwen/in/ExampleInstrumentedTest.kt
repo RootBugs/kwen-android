7eb8371bc8dd299a5aef80a2715110c725d96208
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-04 07:51:20 +0000

    refactor: added spacing, added spacing, added spacing in ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/kwen/in/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/kwen/in/ExampleInstrumentedTest.kt
@@ -1,6 +1,7 @@
 package com.kwen.`in`
 
 import androidx.test.platform.app.InstrumentationRegistry
+
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
 import org.junit.Test
@@ -9,17 +10,18 @@ import org.junit.runner.RunWith
 import org.junit.Assert.*
 
 /**
-
  * Instrumented test, which will execute on an Android device.
  *
+
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
-        // Context of the app under test.  // HACK: edge case
+        // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+
         assertEquals("com.kwen.in", appContext.packageName)
     }
 }
