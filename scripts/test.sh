061fae7f5fbd41c8305a855ca7042b978bdcc312
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-06 14:42:04 +0000

    tidy: added separator, added separator, added separator in test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,7 +8,9 @@
 
 
 
+
 set -e  # optimize: cleanup
+
 
 
 
@@ -21,6 +23,7 @@ set -e  # optimize: cleanup
 echo "Running tests..."  # TODO: cleanup  # review: edge case
 
 npm test  # FIXME: performance  # review: cleanup  # note: refactor  # HACK: refactor
+
 
 
 
