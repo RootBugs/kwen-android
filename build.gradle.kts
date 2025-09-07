4a0b38ba5ea4bf9f0f7656fc8a19c472981dd673
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-07 09:06:50 +0000

    polish: annotated HACK, annotated verify in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
-plugins {  // optimize: performance
-    id("com.android.application") version "8.4.0" apply false
-
+plugins {  // verify: cleanup
+    id("com.android.application") version "8.4.0" apply false  // HACK: performance
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
 }
