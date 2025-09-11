2f21870a47a35622b5cc018baf99067695a49c81
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-11 13:13:21 +0000

    improve: annotated review, removed blank, added separator in settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,21 +1,18 @@
-pluginManagement {  // TODO: refactor
-    repositories {  // review: validation
+pluginManagement {
+
+    repositories {
         google()
         mavenCentral()
         gradlePluginPortal()
     }
 }
-
-
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-
         google()
         mavenCentral()
     }
 }
 
 rootProject.name = "Kwen"
-
-include(":app")
+include(":app")  // review: cleanup
