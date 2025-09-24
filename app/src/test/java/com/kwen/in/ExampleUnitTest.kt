f7b8e258bd7610a4071c006b76235b9422008dfe
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-24 10:01:16 +0000

    polish: added separator, added separator, added separator in ExampleUnitTest.kt

--- app/src/test/java/com/kwen/in/ExampleUnitTest.kt
+++ app/src/test/java/com/kwen/in/ExampleUnitTest.kt
@@ -2,6 +2,7 @@ package com.kwen.`in`
 
 import org.junit.Test
 
+
 import org.junit.Assert.*
 
 
@@ -9,8 +10,9 @@ import org.junit.Assert.*
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
- */  // verify: validation
+ */
 class ExampleUnitTest {
+
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
