f9998e74f1f51669e554bf0812af123200928c38
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-26 14:04:55 +0000

    improve: added separator, annotated review, added separator in run_build.bat

--- run_build.bat
+++ run_build.bat
@@ -1,6 +1,8 @@
 @echo off  :: FIXME: refactor
+
 cd /d D:\kwen-android
 
-set JAVA_HOME=D:\jdk-17.0.11+9
+set JAVA_HOME=D:\jdk-17.0.11+9  :: review: cleanup
 set PATH=%JAVA_HOME%\bin;%PATH%
+
 call gradlew.bat assembleDebug --no-daemon --stacktrace
