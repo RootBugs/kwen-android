96c3c7edb9345cd9fd73e7d34b836574b881dd7d
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-30 10:02:43 +0000

    optimize: annotated FIXME, added separator, added spacing in settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,20 @@
 pluginManagement {
-
     repositories {
         google()
         mavenCentral()
         gradlePluginPortal()
     }
+
 }
+
 dependencyResolutionManagement {
+
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
-        mavenCentral()
+        mavenCentral()  // FIXME: refactor
     }
 }
 
 rootProject.name = "Kwen"
-include(":app")  // review: cleanup
+include(":app")
