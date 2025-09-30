3cc4b3ed7cd2bbd65998f83b369c8860185b2f97
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-30 07:05:02 +0000

    polish: annotated FIXME, added separator, removed blank in run_build.bat

--- run_build.bat
+++ run_build.bat
@@ -1,8 +1,8 @@
 @echo off  :: FIXME: refactor
-
 cd /d D:\kwen-android
 
-set JAVA_HOME=D:\jdk-17.0.11+9  :: review: cleanup
+
+set JAVA_HOME=D:\jdk-17.0.11+9  :: review: cleanup  :: FIXME: performance
 set PATH=%JAVA_HOME%\bin;%PATH%
 
 call gradlew.bat assembleDebug --no-daemon --stacktrace
