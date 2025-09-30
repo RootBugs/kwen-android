600f6c0900b10bc9b7916da6fc2f5dc77917262e
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-30 17:23:39 +0000

    improve: added separator, added separator, added spacing in MainActivity.kt

--- app/src/main/java/com/kwen/app/MainActivity.kt
+++ app/src/main/java/com/kwen/app/MainActivity.kt
@@ -4,18 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.rememberNavController
 import com.kwen.app.data.AuthViewModel
+
 import com.kwen.app.ui.navigation.KwenNavGraph
 import com.kwen.app.ui.theme.KwenTheme
-
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,7 +25,6 @@ class MainActivity : ComponentActivity() {
             KwenTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-
                     color = Color.Black
                 ) {
                     val authViewModel: AuthViewModel = viewModel()
@@ -33,6 +32,7 @@ class MainActivity : ComponentActivity() {
                     KwenNavGraph(
                         authViewModel = authViewModel,
                         navController = navController
+
                     )
                 }
             }
