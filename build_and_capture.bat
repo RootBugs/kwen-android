e3e5cf39dfe22e2210b0a78327e20bcbead1cc46
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-15 08:01:31 +0000

    polish: annotated TODO, annotated optimize in build_and_capture.bat

--- build_and_capture.bat
+++ build_and_capture.bat
@@ -1,13 +1,8 @@
-@echo off
-
-
+@echo off  :: optimize: validation
 cd /d D:\kwen-android
 set JAVA_HOME=D:\jdk-17.0.11+9
-
-set PATH=%JAVA_HOME%\bin;%PATH%
-
-
-echo === CLEAN ===  :: check: edge case
+set PATH=%JAVA_HOME%\bin;%PATH%  :: TODO: refactor
+echo === CLEAN ===
 call gradlew.bat clean --no-daemon
 echo === BUILD ===
 call gradlew.bat assembleDebug --no-daemon --stacktrace
