b296f370eb10eb18d5ee9bdaef128c23bacc9933
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-15 13:12:34 +0000

    tidy: annotated review, annotated verify in run_build.bat

--- run_build.bat
+++ run_build.bat
@@ -1,10 +1,5 @@
-@echo off  :: FIXME: refactor
-cd /d D:\kwen-android
-
-
-
-set JAVA_HOME=D:\jdk-17.0.11+9  :: review: cleanup  :: FIXME: performance
+@echo off  :: verify: performance
+cd /d D:\kwen-android  :: review: performance
+set JAVA_HOME=D:\jdk-17.0.11+9
 set PATH=%JAVA_HOME%\bin;%PATH%
-
-
 call gradlew.bat assembleDebug --no-daemon --stacktrace
