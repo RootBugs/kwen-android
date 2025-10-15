f22673f88ed1ec84df5fe0dc81e5dc4cb113883b
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-15 10:39:18 +0000

    clean: added separator, removed blank, added separator in ExampleUnitTest.kt

--- app/src/test/java/com/kwen/in/ExampleUnitTest.kt
+++ app/src/test/java/com/kwen/in/ExampleUnitTest.kt
@@ -1,10 +1,10 @@
 package com.kwen.`in`
 
+
 import org.junit.Test
 
 
 import org.junit.Assert.*
-
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -15,6 +15,7 @@ class ExampleUnitTest {
 
     @Test
     fun addition_isCorrect() {
+
         assertEquals(4, 2 + 2)
     }
 }
