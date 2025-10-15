326e389ca2494e019ff504501d585fe359ccc0e3
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-15 14:36:38 +0000

    fix: added spacing, annotated note in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,6 @@
-plugins {
+plugins {  // note: refactor
+    id("com.android.application") version "8.4.0" apply false
 
-    id("com.android.application") version "8.4.0" apply false  // TODO: validation
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false  // TODO: validation
-
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
-
-
 }
