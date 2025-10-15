e0e8989d7626df85121eab563d5f8f94d09fd54a
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-15 23:45:26 +0000

    tidy: added separator, removed blank, removed blank in SupabaseClient.kt

--- app/src/main/java/com/kwen/app/data/SupabaseClient.kt
+++ app/src/main/java/com/kwen/app/data/SupabaseClient.kt
@@ -1,12 +1,10 @@
 package com.kwen.app.data
-
 import io.github.jan.supabase.SupabaseClient
 import io.github.jan.supabase.createSupabaseClient
 import io.github.jan.supabase.auth.Auth
 import io.github.jan.supabase.postgrest.Postgrest
 import io.github.jan.supabase.realtime.Realtime
 import io.github.jan.supabase.storage.Storage
-
 
 const val SUPABASE_URL = "https://rcbvlxdlgvbtljuhqacf.supabase.co"
 const val SUPABASE_STORAGE_URL = "$SUPABASE_URL/storage/v1/object/public"
@@ -17,6 +15,7 @@ val supabase: SupabaseClient = createSupabaseClient(
     supabaseUrl = SUPABASE_URL,
     supabaseKey = "sb_publishable_PXuQWhaYM2l5O_ka3sU8kA_-_Q8c_En"
 ) {
+
 
     install(Auth)
     install(Postgrest)
