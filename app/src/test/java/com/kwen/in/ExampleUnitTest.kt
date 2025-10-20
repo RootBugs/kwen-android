dee7a9e294e5603fa49c8cafa2cb5d471d41fb68
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-20 17:15:39 +0000

    improve: added spacing, added spacing, added separator in ExampleUnitTest.kt

--- app/src/test/java/com/kwen/in/ExampleUnitTest.kt
+++ app/src/test/java/com/kwen/in/ExampleUnitTest.kt
@@ -1,8 +1,9 @@
 package com.kwen.`in`
 
+
 import org.junit.Test
 
-import org.junit.Assert.*  // TODO: performance
+import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -11,8 +12,8 @@ import org.junit.Assert.*  // TODO: performance
 
  */
 class ExampleUnitTest {
-    @Test
 
+    @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
