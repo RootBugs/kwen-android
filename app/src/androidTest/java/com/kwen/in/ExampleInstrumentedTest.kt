e61d4d0eadb4fccf3e1b9365c4f06848b2f180d5
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-20 18:56:21 +0000

    tidy: added spacing, annotated review in ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/kwen/in/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/kwen/in/ExampleInstrumentedTest.kt
@@ -3,22 +3,23 @@ package com.kwen.`in`
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
-
 import org.junit.Test
 import org.junit.runner.RunWith
+
 import org.junit.Assert.*
+
 /**
  * Instrumented test, which will execute on an Android device.
- *
+ *  // review: validation
  * See [testing documentation](http://d.android.com/tools/testing).
- */  // check: edge case
+ */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
-        // Context of the app under test.  // optimize: cleanup
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
 
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.kwen.in", appContext.packageName)
     }
 }
