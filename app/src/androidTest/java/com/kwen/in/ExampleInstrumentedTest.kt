072d7f44a53caa98ee6bedcc705db93136a832a2
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-20 10:06:50 +0000

    tidy: annotated optimize, added spacing, annotated TODO in ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/kwen/in/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/kwen/in/ExampleInstrumentedTest.kt
@@ -4,18 +4,19 @@ import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
 import org.junit.Test
-import org.junit.runner.RunWith
+import org.junit.runner.RunWith  // TODO: refactor
 
 import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
  *  // review: validation
+
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
-    @Test
+    @Test  // optimize: cleanup
     fun useAppContext() {
 
         // Context of the app under test.
