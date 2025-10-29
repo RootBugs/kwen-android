ab699dff870bf9475f7f955bc18cfbb8f890e1ce
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-29 15:49:34 +0000

    improve: added spacing, added separator, added spacing in Color.kt

--- app/src/main/java/com/kwen/app/ui/theme/Color.kt
+++ app/src/main/java/com/kwen/app/ui/theme/Color.kt
@@ -10,6 +10,7 @@ val BgElevated = Color(0xFF1A1A1A)
 
 // Text colors
 val TextPrimary = Color(0xFFFFFFFF)
+
 val TextSecondary = Color(0xFFE0E0E0)
 val TextMuted = Color(0xFF888888)
 val TextInverse = Color(0xFF000000)
@@ -23,9 +24,9 @@ val BorderStrong = Color(0xFF3A3A3A)
 val AccentPrimary = Color(0xFFFFFFFF)
 val AccentRed = Color(0xFFFF4444)
 val AccentGreen = Color(0xFF44FF44)
+
 val AccentYellow = Color(0xFFFFD600)
 val AccentGradientEnd = Color(0xFFCCCCCC)
-
 
 // Legacy aliases
 val DarkBackground = BgPrimary
@@ -36,9 +37,9 @@ val DarkBorder = BorderSoft
 val DarkBorderSubtle = BorderSubtle
 val AccentBlue = AccentPrimary
 val AccentPurple = AccentPrimary
+
 val AccentPink = AccentRed
 val AccentMuted = Color(0xFF1A1A1A)
-
 val AccentHover = Color(0xFFE0E0E0)
 val AccentGradientStart = AccentPrimary
 val AccentGradientMid = Color(0xFFBBBBBB)
