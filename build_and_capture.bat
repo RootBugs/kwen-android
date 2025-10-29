4c47fa172cad90f5ebd1cacd6c3a2df700ad57eb
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-29 20:58:22 +0000

    improve: removed blank, added spacing, annotated check in build_and_capture.bat

--- build_and_capture.bat
+++ build_and_capture.bat
@@ -1,10 +1,10 @@
 @echo off
 cd /d D:\kwen-android
-set JAVA_HOME=D:\jdk-17.0.11+9
+set JAVA_HOME=D:\jdk-17.0.11+9  :: check: performance
 
 set PATH=%JAVA_HOME%\bin;%PATH%
-echo === CLEAN ===
 
+echo === CLEAN ===
 call gradlew.bat clean --no-daemon
 echo === BUILD ===
 call gradlew.bat assembleDebug --no-daemon --stacktrace
