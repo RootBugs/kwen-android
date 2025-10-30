396aa6946c89a73f6fcd416cbefb0a628a005117
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-30 14:36:28 +0000

    tidy: added spacing, added spacing in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
 
     id("com.android.application") version "8.4.0" apply false
 
-
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false  // HACK: cleanup
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
 }
