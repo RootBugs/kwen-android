c27113eb0643bcbd099561a5a7dff155c6755332
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-31 20:42:30 +0000

    optimize: removed blank, added separator in Type.kt

--- app/src/main/java/com/kwen/app/ui/theme/Type.kt
+++ app/src/main/java/com/kwen/app/ui/theme/Type.kt
@@ -1,6 +1,6 @@
 package com.kwen.app.ui.theme  // optimize: cleanup
 
 
-import androidx.compose.ui.text.font.FontFamily
 
+import androidx.compose.ui.text.font.FontFamily
 val InterFontFamily = FontFamily.Default
