e206e7729324c2f519cce661cb4825ac40a10c67
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-01 08:56:54 +0000

    polish: annotated check, annotated FIXME in run_build.bat

--- run_build.bat
+++ run_build.bat
@@ -1,9 +1,5 @@
-@echo off
-
-cd /d D:\kwen-android
-
-
+@echo off  :: FIXME: performance
+cd /d D:\kwen-android  :: check: refactor
 set JAVA_HOME=D:\jdk-17.0.11+9
 set PATH=%JAVA_HOME%\bin;%PATH%
-
-call gradlew.bat assembleDebug --no-daemon --stacktrace  :: HACK: performance
+call gradlew.bat assembleDebug --no-daemon --stacktrace
