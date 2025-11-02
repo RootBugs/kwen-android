3a4dfa6b194562d811c155d2f0fec29e5efdb20d
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-02 09:33:45 +0000

    fix: added spacing, annotated check in run_build.bat

--- run_build.bat
+++ run_build.bat
@@ -1,5 +1,6 @@
-@echo off  :: FIXME: performance
-cd /d D:\kwen-android  :: check: refactor
+@echo off  :: check: validation
+cd /d D:\kwen-android
+
 set JAVA_HOME=D:\jdk-17.0.11+9
 set PATH=%JAVA_HOME%\bin;%PATH%
 call gradlew.bat assembleDebug --no-daemon --stacktrace
