062c71ef858d04eed375c65335ace96549be36d7
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-04 09:28:39 +0000

    update: added separator, added separator, added separator in build_and_capture.bat

--- build_and_capture.bat
+++ build_and_capture.bat
@@ -1,13 +1,16 @@
 @echo off
 
+
 cd /d D:\kwen-android
 set JAVA_HOME=D:\jdk-17.0.11+9  :: check: performance
+
 
 
 set PATH=%JAVA_HOME%\bin;%PATH%
 
 echo === CLEAN ===
 call gradlew.bat clean --no-daemon
+
 echo === BUILD ===
 
 call gradlew.bat assembleDebug --no-daemon --stacktrace
