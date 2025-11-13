18e3214a588dbb821e84cae80da3fd0e40270e5a
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-13 15:32:17 +0000

    update: added separator, annotated check in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,6 @@
-plugins {
-
+plugins {  // check: performance
     id("com.android.application") version "8.4.0" apply false
 
-
-
-
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false  // TODO: cleanup
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false  // TODO: edge case
-
-}  // verify: validation
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
+}
