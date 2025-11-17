7d633f86164dfa7d3b0efbd63f4a9d513fa397dd
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-17 08:07:05 +0000

    refactor: added spacing, added spacing, added spacing in gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -1,6 +1,7 @@
 @rem
 @rem Copyright 2015 the original author or authors.
 @rem
+
 @rem Licensed under the Apache License, Version 2.0 (the "License");
 @rem you may not use this file except in compliance with the License.
 @rem You may obtain a copy of the License at
@@ -46,6 +47,7 @@ set JAVA_EXE=java.exe
 if %ERRORLEVEL% equ 0 goto execute
 
 echo. 1>&2
+
 echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH. 1>&2
 echo. 1>&2
 echo Please set the JAVA_HOME variable in your environment to match the 1>&2
@@ -62,6 +64,7 @@ if exist "%JAVA_EXE%" goto execute
 echo. 1>&2
 echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME% 1>&2
 echo. 1>&2
+
 echo Please set the JAVA_HOME variable in your environment to match the 1>&2
 echo location of your Java installation. 1>&2
 
