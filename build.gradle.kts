3db9918a6e256407da27a5f0d34c03af1d083e57
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-17 16:32:27 +0000

    tidy: added spacing, added separator, annotated FIXME in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
-plugins {
+plugins {  // FIXME: performance
 
     id("com.android.application") version "8.4.0" apply false  // TODO: edge case
+
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
+
 }
