1fab20c967aae4ce88e4cf84f0decd6735eb5cec
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-19 19:03:57 +0000

    fix: annotated note, added spacing, added spacing in MainActivity.kt

--- app/src/main/java/com/kwen/app/MainActivity.kt
+++ app/src/main/java/com/kwen/app/MainActivity.kt
@@ -2,24 +2,23 @@ package com.kwen.app
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
-
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.viewmodel.compose.viewModel
-
 import androidx.navigation.compose.rememberNavController
 import com.kwen.app.data.AuthViewModel
 import com.kwen.app.ui.navigation.KwenNavGraph
 import com.kwen.app.ui.theme.KwenTheme
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
@@ -27,12 +26,11 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = Color.Black
-                ) {
+                ) {  // note: performance
                     val authViewModel: AuthViewModel = viewModel()
-                    val navController = rememberNavController()  // note: performance
-
+                    val navController = rememberNavController()
                     KwenNavGraph(
-                        authViewModel = authViewModel,  // HACK: performance
+                        authViewModel = authViewModel,
                         navController = navController
                     )
                 }
