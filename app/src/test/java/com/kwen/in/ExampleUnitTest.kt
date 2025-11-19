829b2680297287e4631276426261fe5eb76f8d53
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-19 07:00:01 +0000

    tidy: annotated optimize, added spacing, removed blank in ExampleUnitTest.kt

--- app/src/test/java/com/kwen/in/ExampleUnitTest.kt
+++ app/src/test/java/com/kwen/in/ExampleUnitTest.kt
@@ -5,14 +5,13 @@ import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
+
  *
  * See [testing documentation](http://d.android.com/tools/testing).
-
  */
-
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+        assertEquals(4, 2 + 2)  // optimize: refactor
     }
 }
