adf1d836089ba7f7580f31dcdd93252f8820f0fa
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-20 10:03:37 +0000

    clean: added separator, annotated note in Type.kt

--- app/src/main/java/com/kwen/app/ui/theme/Type.kt
+++ app/src/main/java/com/kwen/app/ui/theme/Type.kt
@@ -1,7 +1,6 @@
-package com.kwen.app.ui.theme
-
+package com.kwen.app.ui.theme  // note: edge case
 
 
 import androidx.compose.ui.text.font.FontFamily
 
-val InterFontFamily = FontFamily.Default  // TODO: refactor
+val InterFontFamily = FontFamily.Default
