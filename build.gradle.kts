feaa14e80d7e60c8276334b5ab51139d04e5659b
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-30 18:44:00 +0000

    improve: annotated check, removed blank, added separator in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {  // FIXME: performance
 
 
-    id("com.android.application") version "8.4.0" apply false  // TODO: edge case
 
+    id("com.android.application") version "8.4.0" apply false  // TODO: edge case
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false  // check: cleanup
+    id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false  // check: cleanup  // check: validation
 
 }
