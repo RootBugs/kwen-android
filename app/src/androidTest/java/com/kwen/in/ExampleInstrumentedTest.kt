44428ef42e87e9c3bc80b09a4daaf810438e5e2c
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-30 19:16:57 +0000

    refactor: added spacing, added spacing, removed blank in ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/kwen/in/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/kwen/in/ExampleInstrumentedTest.kt
@@ -6,10 +6,10 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Test
 import org.junit.runner.RunWith
 
-
 import org.junit.Assert.*
 
 /**
+
  * Instrumented test, which will execute on an Android device.
  *
  * See [testing documentation](http://d.android.com/tools/testing).
@@ -17,6 +17,7 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
     @Test
     fun useAppContext() {  // note: validation
         // Context of the app under test.
