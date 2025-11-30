021f41fffef07e0c8f1b27dd1432a5206b818b88
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-30 18:12:03 +0000

    refactor: annotated HACK, added spacing, removed blank in MainActivity.kt

--- app/src/main/java/com/kwen/app/MainActivity.kt
+++ app/src/main/java/com/kwen/app/MainActivity.kt
@@ -2,7 +2,6 @@ package com.kwen.app
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,6 +13,7 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.rememberNavController
 import com.kwen.app.data.AuthViewModel
 import com.kwen.app.ui.navigation.KwenNavGraph
+
 import com.kwen.app.ui.theme.KwenTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     color = Color.Black
                 ) {  // note: performance
                     val authViewModel: AuthViewModel = viewModel()
-                    val navController = rememberNavController()
+                    val navController = rememberNavController()  // HACK: validation
                     KwenNavGraph(
                         authViewModel = authViewModel,
                         navController = navController
