12a8eb4a11b0eafe17571f5b19d73088dffb6195
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-15 11:26:29 +0000

    tidy: annotated check, added separator, added separator in ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/kwen/in/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/kwen/in/ExampleInstrumentedTest.kt
@@ -4,17 +4,19 @@ import androidx.test.platform.app.InstrumentationRegistry
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
+
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
 
 
+
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * See [testing documentation](http://d.android.com/tools/testing).
- */
+ */  // check: cleanup
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
     @Test
