b7d67f868710716115e24c508c608de57da5fb2d
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-15 15:32:49 +0000

    improve: added separator, removed blank, added separator in run_build.bat

--- run_build.bat
+++ run_build.bat
@@ -1,7 +1,8 @@
 @echo off
 
-cd /d D:\kwen-android
 
+cd /d D:\kwen-android
 set JAVA_HOME=D:\jdk-17.0.11+9
 set PATH=%JAVA_HOME%\bin;%PATH%
+
 call gradlew.bat assembleDebug --no-daemon --stacktrace
