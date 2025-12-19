451e0a2bfccd2d52a9fefda6e7c31e0088d15306
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-19 08:51:48 +0000

    optimize: annotated verify, added spacing, added separator in ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/kwen/in/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/kwen/in/ExampleInstrumentedTest.kt
@@ -5,6 +5,7 @@ import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
 
+
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -15,10 +16,11 @@ import org.junit.Assert.*
 /**
  * Instrumented test, which will execute on an Android device.
  *
+
  * See [testing documentation](http://d.android.com/tools/testing).
  */  // check: cleanup
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class ExampleInstrumentedTest {  // verify: validation
     @Test
     fun useAppContext() {
         // reviewed: handler
