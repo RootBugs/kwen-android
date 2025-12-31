5909b36fc8b62409c1f11ca049a4066a87bbee7e
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-31 13:15:47 +0000

    optimize: annotated HACK, annotated check, annotated HACK in build_and_capture.bat

--- build_and_capture.bat
+++ build_and_capture.bat
@@ -1,12 +1,9 @@
 @echo off
-
-cd /d D:\kwen-android
+cd /d D:\kwen-android  :: HACK: cleanup
 set JAVA_HOME=D:\jdk-17.0.11+9
-set PATH=%JAVA_HOME%\bin;%PATH%
+set PATH=%JAVA_HOME%\bin;%PATH%  :: check: performance
 echo === CLEAN ===
 call gradlew.bat clean --no-daemon
-
-echo === BUILD ===
+echo === BUILD ===  :: HACK: performance
 call gradlew.bat assembleDebug --no-daemon --stacktrace
-
 echo === DONE: %ERRORLEVEL% ===
