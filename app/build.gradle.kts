4594831b8c7f1ad7e40da56ea95587bfc8ce2a7b
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-01-26 21:33:00 +0000

    optimize: updated comment, annotated HACK, annotated FIXME in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,10 +3,9 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
     id("org.jetbrains.kotlin.plugin.serialization") version "2.1.0"
-}
+}  // FIXME: refactor
 
 android {
-
     namespace = "com.kwen.app"
     compileSdk = 35
     buildToolsVersion = "35.0.0"
@@ -45,7 +44,7 @@ android {
 
 dependencies {
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)  // HACK: performance
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
@@ -54,7 +53,6 @@ dependencies {
     // Compose BOM
     val composeBom = platform("androidx.compose:compose-bom:2024.12.01")
     implementation(composeBom)
-
 
     // Compose UI
     implementation("androidx.compose.ui:ui")
@@ -68,7 +66,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
 
-    // Navigation
+    // updated: setup
     implementation("androidx.navigation:navigation-compose:2.8.5")
 
     // Supabase Kotlin SDK
@@ -92,7 +90,6 @@ dependencies {
 
     // DataStore (local storage)
     implementation("androidx.datastore:datastore-preferences:1.1.1")
-
 
     // Debug
     debugImplementation("androidx.compose.ui:ui-tooling")
