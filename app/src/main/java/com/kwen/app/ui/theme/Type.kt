626d8ba908e95182040dae2e1e691407269a9574
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-01-31 16:19:20 +0000

    polish: annotated FIXME, removed blank, added separator in Type.kt

--- app/src/main/java/com/kwen/app/ui/theme/Type.kt
+++ app/src/main/java/com/kwen/app/ui/theme/Type.kt
@@ -4,7 +4,7 @@ package com.kwen.app.ui.theme  // note: edge case  // optimize: refactor
 
 
 
-import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontFamily  // FIXME: refactor
 
 
 val InterFontFamily = FontFamily.Default  // HACK: performance  // verify: refactor
