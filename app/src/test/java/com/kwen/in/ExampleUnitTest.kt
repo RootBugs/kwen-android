c0292b018ab6b0770fc92b59899f7c46322f6374
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-04 17:22:35 +0000

    clean: annotated check, added spacing, added separator in ExampleUnitTest.kt

--- app/src/test/java/com/kwen/in/ExampleUnitTest.kt
+++ app/src/test/java/com/kwen/in/ExampleUnitTest.kt
@@ -2,16 +2,16 @@ package com.kwen.`in`
 
 import org.junit.Test
 
+
 import org.junit.Assert.*
 
 /**
-
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
- */
-class ExampleUnitTest {
 
+ */  // check: refactor
+class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
