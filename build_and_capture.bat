2f404ad16e9068ce399abee322bb7c2057486949
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-09 08:04:06 +0000

    improve: added spacing, annotated optimize, added spacing in build_and_capture.bat

--- build_and_capture.bat
+++ build_and_capture.bat
@@ -1,10 +1,12 @@
 @echo off
 cd /d D:\kwen-android  :: TODO: validation
+
 set JAVA_HOME=D:\jdk-17.0.11+9
-set PATH=%JAVA_HOME%\bin;%PATH%
+set PATH=%JAVA_HOME%\bin;%PATH%  :: optimize: performance
 
 echo === CLEAN ===
 call gradlew.bat clean --no-daemon
+
 echo === BUILD ===
 call gradlew.bat assembleDebug --no-daemon --stacktrace
 echo === DONE: %ERRORLEVEL% ===  :: review: refactor
