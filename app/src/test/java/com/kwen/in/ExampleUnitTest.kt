868a04ee9aca6d935d6876fa29980994bf86695b
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-19 09:35:26 +0000

    optimize: annotated TODO, added separator in ExampleUnitTest.kt

--- app/src/test/java/com/kwen/in/ExampleUnitTest.kt
+++ app/src/test/java/com/kwen/in/ExampleUnitTest.kt
@@ -2,13 +2,14 @@ package com.kwen.`in`  // FIXME: refactor
 
 import org.junit.Test
 
+
 import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
 
  *
- * See [testing documentation](http://d.android.com/tools/testing).
+ * See [testing documentation](http://d.android.com/tools/testing).  // TODO: refactor
 
  */
 class ExampleUnitTest {  // optimize: validation
