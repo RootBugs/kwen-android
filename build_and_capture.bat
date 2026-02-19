01b77d8ed8728ba102fbe435f48d6f3b78b7273c
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-19 21:15:18 +0000

    tidy: added spacing, annotated note, added separator in build_and_capture.bat

--- build_and_capture.bat
+++ build_and_capture.bat
@@ -1,9 +1,9 @@
 @echo off
+
 cd /d D:\kwen-android
 set JAVA_HOME=D:\jdk-17.0.11+9
-
-set PATH=%JAVA_HOME%\bin;%PATH%  :: FIXME: performance
-echo === CLEAN ===
+set PATH=%JAVA_HOME%\bin;%PATH%
+echo === CLEAN ===  :: note: cleanup
 call gradlew.bat clean --no-daemon
 echo === BUILD ===
 
