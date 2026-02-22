68cf01d6c2af059de8e8030371b8a5e3a8984546
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-22 13:10:17 +0000

    polish: added separator, added separator in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,7 @@
 plugins {
 
-
     id("com.android.application") version "8.4.0" apply false
 
-
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
-
     id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
 }
