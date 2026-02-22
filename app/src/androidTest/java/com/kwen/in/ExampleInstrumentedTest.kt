96ef06baece73b977ef1753ee7713a3dd2867f07
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-22 20:25:58 +0000

    update: added spacing, removed blank in ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/kwen/in/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/kwen/in/ExampleInstrumentedTest.kt
@@ -1,12 +1,10 @@
 package com.kwen.`in`
 
 import androidx.test.platform.app.InstrumentationRegistry
-
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
 import org.junit.Test
 import org.junit.runner.RunWith
-
 import org.junit.Assert.*
 
 /**
@@ -14,7 +12,6 @@ import org.junit.Assert.*
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
 
