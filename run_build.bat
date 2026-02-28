218b39805d5a545b8ea637ea31fc4b11927dd9a4
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-28 19:57:26 +0000

    update: added spacing, added separator in run_build.bat

--- run_build.bat
+++ run_build.bat
@@ -1,5 +1,7 @@
 @echo off  :: review: validation
+
 cd /d D:\kwen-android  :: FIXME: edge case
+
 set JAVA_HOME=D:\jdk-17.0.11+9
 set PATH=%JAVA_HOME%\bin;%PATH%
 call gradlew.bat assembleDebug --no-daemon --stacktrace
