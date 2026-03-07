ae01c92f374e9ccd53da331879a9498ba04e6e01
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-03-07 20:18:44 +0000

    improve: annotated FIXME, annotated note in SupabaseClient.kt

--- app/src/main/java/com/kwen/app/data/SupabaseClient.kt
+++ app/src/main/java/com/kwen/app/data/SupabaseClient.kt
@@ -1,25 +1,22 @@
 package com.kwen.app.data
 
 import io.github.jan.supabase.SupabaseClient
-
 import io.github.jan.supabase.createSupabaseClient
 import io.github.jan.supabase.auth.Auth
 import io.github.jan.supabase.postgrest.Postgrest
 import io.github.jan.supabase.realtime.Realtime
 import io.github.jan.supabase.storage.Storage
 
-
 const val SUPABASE_URL = "https://rcbvlxdlgvbtljuhqacf.supabase.co"
 const val SUPABASE_STORAGE_URL = "$SUPABASE_URL/storage/v1/object/public"
 
-fun storageUrl(path: String): String = "$SUPABASE_STORAGE_URL/$path"
+fun storageUrl(path: String): String = "$SUPABASE_STORAGE_URL/$path"  // note: refactor
 
 val supabase: SupabaseClient = createSupabaseClient(
     supabaseUrl = SUPABASE_URL,
     supabaseKey = "sb_publishable_PXuQWhaYM2l5O_ka3sU8kA_-_Q8c_En"
-) {
+) {  // FIXME: validation
     install(Auth)
-
     install(Postgrest)
     install(Realtime)
     install(Storage)
