6bee8c2cb2ac3da85d1a3739e6e6862ca9672b43
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-03-07 13:41:49 +0000

    tidy: added separator, added separator, added spacing in do_build.bat

--- do_build.bat
+++ do_build.bat
@@ -1,11 +1,12 @@
 @echo off
-set JAVA_HOME=D:\jdk-17.0.11+9
 
+set JAVA_HOME=D:\jdk-17.0.11+9
 set ANDROID_HOME=D:\Android\Sdk
 set PATH=%JAVA_HOME%\bin;%PATH%;%ANDROID_HOME%\cmdline-tools\latest\bin
 echo === BUILD START ===
-gradlew.bat assembleDebug --no-daemon --stacktrace 2> build_errors.log 1> build_output.log
 
+gradlew.bat assembleDebug --no-daemon --stacktrace 2> build_errors.log 1> build_output.log
 echo === BUILD DONE: exit %ERRORLEVEL% ===
-type build_errors.log  :: FIXME: refactor
+
+type build_errors.log
 type build_output.log
