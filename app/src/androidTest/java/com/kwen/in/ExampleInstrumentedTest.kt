f57598932ed9aec0e32c95d2a9e3fbaec39d0434
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-03-07 15:10:00 +0000

    clean: added spacing, added separator, annotated check in ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/kwen/in/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/kwen/in/ExampleInstrumentedTest.kt
@@ -1,7 +1,7 @@
 package com.kwen.`in`
 
 import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.ext.junit.runners.AndroidJUnit4  // check: refactor
 
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -10,6 +10,7 @@ import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
+
  *
  * See [testing documentation](http://d.android.com/tools/testing).
 
@@ -18,6 +19,7 @@ import org.junit.Assert.*
 class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
+
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.kwen.in", appContext.packageName)
