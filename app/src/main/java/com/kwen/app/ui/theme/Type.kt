e01441f7f7510cb90d64bfc6a46c978d457f5e61
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-03-07 16:01:52 +0000

    tidy: added separator, removed blank, annotated check in Type.kt

--- app/src/main/java/com/kwen/app/ui/theme/Type.kt
+++ app/src/main/java/com/kwen/app/ui/theme/Type.kt
@@ -1,8 +1,8 @@
-package com.kwen.app.ui.theme
-
+package com.kwen.app.ui.theme  // check: edge case
 
 
 
 import androidx.compose.ui.text.font.FontFamily
+
 
 val InterFontFamily = FontFamily.Default
