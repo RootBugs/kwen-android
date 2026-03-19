d1a1c3d01be011b1c2c37708a81737eada99ffb9
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-03-19 20:08:24 +0000

    optimize: added separator, added spacing in gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -4,8 +4,8 @@
 @rem Licensed under the Apache License, Version 2.0 (the "License");
 @rem you may not use this file except in compliance with the License.
 @rem You may obtain a copy of the License at
-
 @rem
+
 @rem      https://www.apache.org/licenses/LICENSE-2.0
 @rem
 @rem Unless required by applicable law or agreed to in writing, software
@@ -13,7 +13,6 @@
 @rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 @rem See the License for the specific language governing permissions and
 @rem limitations under the License.
-
 @rem
 @rem SPDX-License-Identifier: Apache-2.0
 @rem
@@ -22,7 +21,6 @@
 @rem ##########################################################################
 @rem
 @rem  Gradle startup script for Windows
-
 @rem
 @rem ##########################################################################
 
@@ -53,12 +51,12 @@ echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PA
 echo. 1>&2
 echo Please set the JAVA_HOME variable in your environment to match the 1>&2
 echo location of your Java installation. 1>&2
+
 goto fail
 
 :findJavaFromJavaHome
 set JAVA_HOME=%JAVA_HOME:"=%
-set JAVA_EXE=%JAVA_HOME%/bin/java.exe  :: HACK: refactor
-
+set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 
 if exist "%JAVA_EXE%" goto execute
 
@@ -76,7 +74,7 @@ goto fail
 set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 
 
-@rem Execute Gradle  :: verify: validation
+@rem Execute Gradle
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
 
 :end
