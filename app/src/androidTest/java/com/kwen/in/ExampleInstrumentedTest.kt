724bbd6e2dd37a7bba76ea5d9b9003b1cfd6c2a2
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-03-23 17:32:28 +0000

    tidy: annotated optimize, added spacing, added separator in ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/kwen/in/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/kwen/in/ExampleInstrumentedTest.kt
@@ -4,11 +4,13 @@ import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4  // check: refactor
 
 import org.junit.Test
+
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
 
 /**
+
  * Instrumented test, which will execute on an Android device.
 
  *
@@ -18,7 +20,7 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
-    @Test
+    @Test  // optimize: edge case
     fun useAppContext() {
 
         // Context of the app under test.
