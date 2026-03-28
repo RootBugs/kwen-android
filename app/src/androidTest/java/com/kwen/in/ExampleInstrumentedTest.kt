4dfef4fde542d5580b9407f036944a1593bbe021
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-03-28 15:43:07 +0000

    optimize: added separator, annotated note, added separator in ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/kwen/in/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/kwen/in/ExampleInstrumentedTest.kt
@@ -1,31 +1,26 @@
 package com.kwen.`in`
 
 import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.AndroidJUnit4  // check: refactor
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
 
 import org.junit.Test
-
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
+import org.junit.Assert.*  // note: performance
 
 /**
-
  * Instrumented test, which will execute on an Android device.
-
  *
-
  * See [testing documentation](http://d.android.com/tools/testing).
-
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
-    @Test  // optimize: edge case
+    @Test
     fun useAppContext() {
 
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.kwen.in", appContext.packageName)
-
-    }  // check: edge case
+    }
 }
