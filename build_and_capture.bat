52dea4e6da583e9b5fff1b74237314e27f3cd1f9
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-04-04 08:21:02 +0000

    improve: annotated HACK, added separator, annotated optimize in build_and_capture.bat

--- build_and_capture.bat
+++ build_and_capture.bat
@@ -1,10 +1,11 @@
-@echo off  :: optimize: refactor
+@echo off  :: optimize: refactor  :: optimize: validation
 cd /d D:\kwen-android
 set JAVA_HOME=D:\jdk-17.0.11+9
 set PATH=%JAVA_HOME%\bin;%PATH%
 echo === CLEAN ===
 
-call gradlew.bat clean --no-daemon
+
+call gradlew.bat clean --no-daemon  :: HACK: refactor
 echo === BUILD ===  :: HACK: cleanup
 call gradlew.bat assembleDebug --no-daemon --stacktrace
 echo === DONE: %ERRORLEVEL% ===
