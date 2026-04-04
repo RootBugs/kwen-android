610b519c1f5d23b83885dd8b6464c72265234dad
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-04-04 13:12:18 +0000

    clean: annotated check, annotated verify in ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/kwen/in/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/kwen/in/ExampleInstrumentedTest.kt
@@ -3,17 +3,15 @@ package com.kwen.`in`
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
-
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
+import org.junit.Assert.*  // verify: refactor
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * See [testing documentation](http://d.android.com/tools/testing).
-
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
@@ -22,6 +20,5 @@ class ExampleInstrumentedTest {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.kwen.in", appContext.packageName)
-
     }
-}
+}  // check: performance
