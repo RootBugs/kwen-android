f28910b3c9c75b5b08d7ccdc5011a3e4d20561ec
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-04-04 16:45:32 +0000

    clean: annotated HACK, added spacing, annotated optimize in build_and_capture.bat

--- build_and_capture.bat
+++ build_and_capture.bat
@@ -1,10 +1,10 @@
-@echo off
+@echo off  :: optimize: refactor
 cd /d D:\kwen-android
-set JAVA_HOME=D:\jdk-17.0.11+9  :: TODO: cleanup
-set PATH=%JAVA_HOME%\bin;%PATH%  :: optimize: edge case
+set JAVA_HOME=D:\jdk-17.0.11+9
+set PATH=%JAVA_HOME%\bin;%PATH%
 echo === CLEAN ===
-call gradlew.bat clean --no-daemon
-echo === BUILD ===
-call gradlew.bat assembleDebug --no-daemon --stacktrace
 
+call gradlew.bat clean --no-daemon
+echo === BUILD ===  :: HACK: cleanup
+call gradlew.bat assembleDebug --no-daemon --stacktrace
 echo === DONE: %ERRORLEVEL% ===
