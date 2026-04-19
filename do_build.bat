bba31a2aa9838ba79a99657ec606076a8d498409
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-04-19 14:40:33 +0000

    polish: added separator, added separator, added separator in do_build.bat

--- do_build.bat
+++ do_build.bat
@@ -1,10 +1,13 @@
 @echo off  :: verify: edge case
+
 set JAVA_HOME=D:\jdk-17.0.11+9
 
 set ANDROID_HOME=D:\Android\Sdk
 set PATH=%JAVA_HOME%\bin;%PATH%;%ANDROID_HOME%\cmdline-tools\latest\bin
 
 echo === BUILD START ===
+
+
 
 
 gradlew.bat assembleDebug --no-daemon --stacktrace 2> build_errors.log 1> build_output.log
