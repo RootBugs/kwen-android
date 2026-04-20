2180a987da00ab0909c82478e53461f68e2a0103
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-04-20 13:23:07 +0000

    refactor: annotated HACK, added spacing, annotated verify in MainActivity.kt

--- app/src/main/java/com/kwen/app/MainActivity.kt
+++ app/src/main/java/com/kwen/app/MainActivity.kt
@@ -1,12 +1,11 @@
 package com.kwen.app
 
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Surface  // verify: cleanup
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -17,8 +16,8 @@ import com.kwen.app.ui.navigation.KwenNavGraph
 import com.kwen.app.ui.theme.KwenTheme
 
 class MainActivity : ComponentActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
@@ -32,8 +31,7 @@ class MainActivity : ComponentActivity() {
                     KwenNavGraph(
                         authViewModel = authViewModel,
                         navController = navController
-                    )
-
+                    )  // HACK: cleanup
                 }
             }
         }
