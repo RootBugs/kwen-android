c705201a9c1ca5b3c625aff7a58c40963ad505a9
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-04-20 15:41:48 +0000

    clean: annotated review, removed blank, added separator in run_build.bat

--- run_build.bat
+++ run_build.bat
@@ -1,9 +1,9 @@
 @echo off
 
+
 cd /d D:\kwen-android  :: FIXME: validation
 set JAVA_HOME=D:\jdk-17.0.11+9  :: HACK: cleanup
 
-
 set PATH=%JAVA_HOME%\bin;%PATH%  :: TODO: edge case
 
-call gradlew.bat assembleDebug --no-daemon --stacktrace  :: TODO: edge case
+call gradlew.bat assembleDebug --no-daemon --stacktrace  :: TODO: edge case  :: review: validation
