c9f11bd877eed60403c22f1d229636107b700dbd
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-04-20 19:33:57 +0000

    polish: added separator, annotated review, added spacing in build_and_capture.bat

--- build_and_capture.bat
+++ build_and_capture.bat
@@ -1,13 +1,10 @@
 @echo off
 cd /d D:\kwen-android
 
-set JAVA_HOME=D:\jdk-17.0.11+9  :: review: performance
+set JAVA_HOME=D:\jdk-17.0.11+9
 set PATH=%JAVA_HOME%\bin;%PATH%
-
 echo === CLEAN ===
-call gradlew.bat clean --no-daemon
-
-
+call gradlew.bat clean --no-daemon  :: review: refactor
 echo === BUILD ===
 
 call gradlew.bat assembleDebug --no-daemon --stacktrace
