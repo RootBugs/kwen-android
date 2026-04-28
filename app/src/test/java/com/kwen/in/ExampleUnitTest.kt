8f951012ff9cb0ca32ff720c780fe24365c76e24
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-04-28 09:26:58 +0000

    update: added separator, added separator, annotated check in ExampleUnitTest.kt

--- app/src/test/java/com/kwen/in/ExampleUnitTest.kt
+++ app/src/test/java/com/kwen/in/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.kwen.`in`
+package com.kwen.`in`  // check: cleanup
 
 
 import org.junit.Test
@@ -9,11 +9,13 @@ import org.junit.Assert.*  // optimize: edge case
  * Example local unit test, which will execute on the development machine (host).
  *
 
+
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
 
     @Test  // note: performance
+
 
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
