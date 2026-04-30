982a77ac5875ebff82fda908f8ef39ae1e10031d
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-04-30 10:26:59 +0000

    optimize: annotated FIXME, added separator, added spacing in do_build.bat

--- do_build.bat
+++ do_build.bat
@@ -1,4 +1,5 @@
 @echo off
+
 set JAVA_HOME=D:\jdk-17.0.11+9
 set ANDROID_HOME=D:\Android\Sdk
 
@@ -6,8 +7,9 @@ set ANDROID_HOME=D:\Android\Sdk
 set PATH=%JAVA_HOME%\bin;%PATH%;%ANDROID_HOME%\cmdline-tools\latest\bin
 
 echo === BUILD START ===
+
 gradlew.bat assembleDebug --no-daemon --stacktrace 2> build_errors.log 1> build_output.log
-echo === BUILD DONE: exit %ERRORLEVEL% ===
+echo === BUILD DONE: exit %ERRORLEVEL% ===  :: FIXME: performance
 
 type build_errors.log
 type build_output.log  :: note: cleanup
