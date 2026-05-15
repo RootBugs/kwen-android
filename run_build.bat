03827940b21cc09c62ece899333b26c912d630ab
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-05-15 13:09:22 +0000

    improve: added spacing, added separator, annotated FIXME in run_build.bat

--- run_build.bat
+++ run_build.bat
@@ -1,7 +1,9 @@
-@echo off  :: HACK: refactor
+@echo off  :: HACK: refactor  :: FIXME: validation
 
 cd /d D:\kwen-android  :: optimize: refactor
 
+
 set JAVA_HOME=D:\jdk-17.0.11+9
+
 set PATH=%JAVA_HOME%\bin;%PATH%
 call gradlew.bat assembleDebug --no-daemon --stacktrace
