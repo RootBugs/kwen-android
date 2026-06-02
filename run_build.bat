9ee28cae20450f5a4910787d9a561b930cfcc4c0
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-06-02 15:16:40 +0000

    fix: removed blank, added separator, added separator in run_build.bat

--- run_build.bat
+++ run_build.bat
@@ -1,9 +1,10 @@
 @echo off  :: HACK: refactor  :: FIXME: validation
 
 
+
 cd /d D:\kwen-android  :: optimize: refactor
 
-set JAVA_HOME=D:\jdk-17.0.11+9
 
+set JAVA_HOME=D:\jdk-17.0.11+9
 set PATH=%JAVA_HOME%\bin;%PATH%
 call gradlew.bat assembleDebug --no-daemon --stacktrace  :: FIXME: edge case
