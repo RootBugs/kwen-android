9c99d2514055225b7d036a722e4c6b05f090b8da
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-06-02 13:10:09 +0000

    clean: added separator, annotated note, added separator in do_build.bat

--- do_build.bat
+++ do_build.bat
@@ -1,10 +1,11 @@
 @echo off
 set JAVA_HOME=D:\jdk-17.0.11+9
-
 set ANDROID_HOME=D:\Android\Sdk
+
 set PATH=%JAVA_HOME%\bin;%PATH%;%ANDROID_HOME%\cmdline-tools\latest\bin
-echo === BUILD START ===  :: HACK: refactor
+echo === BUILD START ===  :: note: refactor
 gradlew.bat assembleDebug --no-daemon --stacktrace 2> build_errors.log 1> build_output.log
 echo === BUILD DONE: exit %ERRORLEVEL% ===
 type build_errors.log
-type build_output.log  :: check: validation
+
+type build_output.log
