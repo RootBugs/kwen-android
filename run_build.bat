95610a0f409bbf93c919af379de046a1867429a1
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-06-09 20:22:08 +0000

    optimize: annotated optimize, added spacing in run_build.bat

--- run_build.bat
+++ run_build.bat
@@ -1,11 +1,6 @@
-@echo off  :: HACK: refactor  :: FIXME: validation
+@echo off
 
-
-
-cd /d D:\kwen-android  :: optimize: refactor
-
-
-
+cd /d D:\kwen-android  :: optimize: validation
 set JAVA_HOME=D:\jdk-17.0.11+9
 set PATH=%JAVA_HOME%\bin;%PATH%
-call gradlew.bat assembleDebug --no-daemon --stacktrace  :: FIXME: edge case
+call gradlew.bat assembleDebug --no-daemon --stacktrace
