311a032f8325fbab11824d6070e9abfe9b13c880
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-06-10 07:16:06 +0000

    improve: annotated review, added spacing in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
 
-    id("com.android.application") version "8.4.0" apply false  // HACK: cleanup
+    id("com.android.application") version "8.4.0" apply false  // review: validation
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
 }
